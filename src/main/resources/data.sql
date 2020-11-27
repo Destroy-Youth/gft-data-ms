d020e36cc56b0dfabf781e2695b5d008f0eb8941
Equihua <luis.herrerae@outlook.com>
2020-11-27 16:30:34 -0600

    data updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,6 +4,6 @@ insert INTO clients (client_id, first_name, last_name, sex, birthdate) VALUES
   (1, 'Wes', 'Bos', 'm', '2020-06-10 00:00:00.00')
   ;
 
-insert INTO accounts (account_id, account_number, account_type, client_id) VALUES
-  (1, '1384318135816', 'premiere', 1)
+insert INTO accounts (account_id, account_number, balance, account_type, client_id) VALUES
+  (1, '1384318135816', '8542.12', 'premiere', 1)
   ;
