c0f7f417b4f5f7ad39a81ca6ee56a41fb215b083
Equihua <luis.herrerae@outlook.com>
2020-11-26 22:31:39 -0600

    H2 config

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+
+
+merge INTO persons (person_id, first_name, last_name, mail, sex, birthdate) VALUES
+  (1, 'Wes', 'Bos', 'wes.bos@mail.com', 'm', '2020-06-10 00:00:00.00');
+
+merge INTO hobbies (hobbie_id, name) VALUES
+  (1, 'Coding'),
+  (2, 'Videogames'),
+  (3, 'Reading'),
+  (4, 'Pretending to be a plant')
+  ;
